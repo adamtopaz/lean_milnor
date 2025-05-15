ef5c628a603e9203e09cfee981588d341beaf0b1
adamtopaz <github@adamtopaz.com>
2025-05-15 10:12:40 -0600

    initial commit

---
+++ LeanMilnor.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanMilnor` library.
+-- Import modules here that should be built as part of the library.
+import «LeanMilnor».Basic
---
+++ lakefile.lean
@@ -0,0 +1,17 @@
+import Lake
+open Lake DSL
+
+package «lean_milnor» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩
+  ]
+  -- add any additional package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «LeanMilnor» where
+  -- add any library configuration options here
